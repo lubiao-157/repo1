60b7099d8eb561446fdc25f657641d578f220d4b
chenli <chenli@qq.com>
2019-11-26 15:16:46 +0800

    第一次提交

---
+++ git-test/src/com/kgc/demo/test.java
@@ -0,0 +1,12 @@
+package com.kgc.demo;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("牛高");
+        System.out.println("牛高");
+        System.out.println("牛高");
+        System.out.println("牛高");
+        System.out.println("牛高");
+    }
+}
